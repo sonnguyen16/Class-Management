68795ce2a1c26211760f9bcc008cd2d8706c99cf
sonnguyen16 <sonnv2212@gmail.com>
2023-11-12 20:46:59 +0700

    update

--- project/settings.py
+++ project/settings.py
@@ -78,10 +78,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
