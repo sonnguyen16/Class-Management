e7f6934f502bf950b0ba2343be140dd56ca72670
sonnguyen16 <sonnv2212@gmail.com>
2023-11-12 22:53:10 +0700

    update

--- project/settings.py
+++ project/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.app'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static")]
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "app/staticfiles")
