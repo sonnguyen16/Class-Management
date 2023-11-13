b60b267c1e06465d08af042cc5768e957c9929eb
sonnguyen16 <sonnv2212@gmail.com>
2023-11-13 08:02:05 +0700

    update static root

--- project/settings.py
+++ project/settings.py
@@ -71,8 +71,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.app'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'app/static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "app/staticfiles")
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -122,8 +123,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
