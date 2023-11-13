0ead40f554bde355ffae13d92c3926e15291fd0f
sonnguyen16 <sonnv2212@gmail.com>
2023-11-13 08:23:33 +0700

    update url

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
