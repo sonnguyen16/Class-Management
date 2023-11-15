c85bcc9fd875c75ac20f98fa496c4a69de271bfa
Trương An Khang <ankhang897979@gmail.com>
2023-11-15 11:19:12 +0700

    update useradmin

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from django.forms import Textarea
 from django.utils.html import format_html
 from .models import Class, User, Attendance, Homework, DoHomework
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django.contrib.auth.forms import UserChangeForm
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = ('username', 'get_role')
+    add_form_template = 'admin/auth/user/add_form.html'
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'role'),
+        }),
+    )
+    def get_role(self, obj):
+        return obj.get_role_display()
+
+    get_role.short_description = 'Role'
 
 class ClassAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'created_by']
@@ -24,12 +43,22 @@ class AttendanceAdmin(admin.ModelAdmin):
 
 class HomeworkAdmin(admin.ModelAdmin):
     list_display = ['id', 'class_id', 'title', 'description', 'file']
-    search_fields = ['title', 'class_id__name']
+    search_fields = ['title', 'class_id__class_name']  # Sửa thành 'class_id__class_name'
+
     
 class DoHomeworkAdmin(admin.ModelAdmin):
     list_display = ['user_id', 'homework_id', 'file', 'score', 'comment']
     search_fields = ['user_id__email', 'homework_id__title']
 
+# Đăng ký custom UserAdmin
+# Đăng ký User trước khi hủy đăng ký
+admin.site.register(User, UserAdmin)
+
+# Hủy đăng ký User
+admin.site.unregister(User)
+
+# Đăng ký custom UserAdmin
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Class, ClassAdmin)
 admin.site.register(Attendance, AttendanceAdmin)
 admin.site.register(Homework, HomeworkAdmin)
--- app/models.py
+++ app/models.py
@@ -1,39 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-class User(AbstractUser):
+class User(models.Model):
+    # Thêm các trường vào đây
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(max_length=254, blank=True)
     role_choices = [
         (0, 'Giáo viên'),
         (1, 'Sinh viên'),
     ]
     role = models.IntegerField(choices=role_choices)
-
-    # Đặt related_name để tránh xung đột với auth.User
-    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
-
-    class Meta:
-        permissions = [
-            ("co_the_lam_gi_do", "Có thể làm gì đó"),
-        ]
+    def __str__(self):
+        return self.username
 
 class Class(models.Model):
     name = models.CharField(max_length=200)
     created_by = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
 
 class Attendance(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     time = models.DateTimeField()
 
 class Homework(models.Model):
-    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='homeworks')
     title = models.CharField(max_length=200)
     description = models.TextField()
     file = models.FileField(upload_to='uploads/homeworks/')
+
+    def __str__(self):
+        return self.title
 
 class DoHomework(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     homework = models.ForeignKey(Homework, on_delete=models.CASCADE)
     file = models.FileField(upload_to='uploads/do_homeworks/')
-    score = models.IntegerField()
+    score = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True, default=None, validators=[MinValueValidator(0), MaxValueValidator(10)])
     comment = models.TextField()
+    def __str__(self):
+        return self.title
